25835b3d79223ef6a58165af669846aa59fa32fa
sumanpanjiyar <samirpanjiyar4@gmail.com>
2026-01-08 13:05:45 +0545

    updated task6

--- task6.sh
+++ task6.sh
@@ -5,5 +5,5 @@ set -x
 if  id -u deployuser &>/dev/null  ; then
    sudo userdel -r deployuser
 fi
-useradd -m -s /usr/sbin/nologin -p $(openssl passwd -1 suman) deployuser
+useradd -m -S /usr/sbin/nologin -p $(openssl passwd -1 suman) deployuser
 
