8ef7942cf9438cca92fe7c1e12484ea00eea1882
sumanpanjiyar <samirpanjiyar4@gmail.com>
2026-01-08 13:29:00 +0545

    asda

--- storage.sh
+++ storage.sh
@@ -1,6 +1,6 @@
 #Write a shell script to find out disk use by /var/lib/jenkins and write the result to result.txt
 #!/usr/bin/env bash
-set -euo pipefail
+#set -euo pipefail
 set -x
 du -sh /var/lib/jenkins > result.txt
 
